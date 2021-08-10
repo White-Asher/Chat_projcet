65da23357a7077c5dd7edce2aca58d72de8f9c31
Jae-yoon Han <protoss1207@gmail.com>
2021-08-10 15:11:30 +0900

    Update client.py

--- client.py
+++ client.py
@@ -1,8 +1,8 @@
 import socket   # 소켓을 사용하기 위해 socket을 import함
 import threading    # 송신, 수신 쓰레드 분리하기 위해 threading을 import함
 
-HOST = '192.168.35.242' # 연결할 서버 주소 입력
-PORT = 9998 # 소켓 통신할 포트 입력
+HOST = '' # 연결할 서버 주소 입력
+PORT =  # 소켓 통신할 포트 입력
 
 cs = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # 클라이언스 소켓 생성 (첫번째 인자: 패밀리(주소체계:IPv4), 두번째인자: 소켓타입(스트림소켓))
 cs.connect((HOST, PORT)) # 생성된 클라이언트 소켓함수에 (호스트와, 포트값 정보 입력)
